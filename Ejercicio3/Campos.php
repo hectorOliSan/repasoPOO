67dbff3d006f1c11aac34cbc78182d61a7e2cd58
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-12-13 22:57:32 +0000

    Create Campos.php

---
+++ Ejercicio3/Campos.php
@@ -0,0 +1,13 @@
+<?php
+include "CampoHTML.php";
+include "CampoTexto.php";
+include "CampoEmail.php";
+include "CampoPassword.php";
+include "CampoNumber.php";
+include "CampoDateTime.php";
+include "CampoSelect.php";
+include "CampoRadio.php";
+include "CampoCheck.php";
+include "CampoFile.php";
+include "CampoTextArea.php";
+include "CampoSubmit.php";
